36bbf7b8fe147a7ffbe2fe1e9b83862f26d59bcf
Mimansa Sahay <mimansa192@gmail.com>
2025-05-05 17:44:37 +0530

    changed bulk.js

--- bulk.js
+++ bulk.js
@@ -10,7 +10,7 @@ async function scrape() {
   const browser = await puppeteer.launch({
     headless: true, 
     defaultViewport: { width: 1920, height: 1080 }
-  });
+  }); 
   
   try {
     const page = await browser.newPage();
